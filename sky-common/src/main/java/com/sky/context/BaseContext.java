59657a4a67aa0157aca1bb4a9e644588bebf40c4
Ruby-syc <heraphael123@163.com>
2026-04-07 22:57:26 +0800

    公共字段自动填充代码实现

--- sky-common/src/main/java/com/sky/context/BaseContext.java
+++ sky-common/src/main/java/com/sky/context/BaseContext.java
@@ -3,6 +3,7 @@ package com.sky.context;
 public class BaseContext {
 
     public static ThreadLocal<Long> threadLocal = new ThreadLocal<>();
+    public static Object getCurrentId;
 
     public static void setCurrentId(Long id) {
         threadLocal.set(id);
